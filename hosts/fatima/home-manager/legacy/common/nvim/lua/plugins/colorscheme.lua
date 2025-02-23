9c685ed1d6d09f42df242360383a0f1fa9a02988
Sevenings <lvlassis.2@gmail.com>
2025-02-23 20:33:53 -0300

    fix(nvim-everforest): alterado para variante hard

--- hosts/fatima/home-manager/legacy/common/nvim/lua/plugins/colorscheme.lua
+++ hosts/fatima/home-manager/legacy/common/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,9 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
+      require('everforest').setup({
+        background = "hard"
+      })
       -- load the colorscheme here
       vim.cmd([[colorscheme everforest]])
     end,
