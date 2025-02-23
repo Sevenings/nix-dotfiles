076fac26dbed8b6313f5b1a19d8837bb99c6cfdb
Sevenings <lvlassis.2@gmail.com>
2025-02-23 20:27:56 -0300

    feat(nvim): definido o tema para everforest

--- hosts/fatima/home-manager/legacy/common/nvim/lua/plugins/colorscheme.lua
+++ hosts/fatima/home-manager/legacy/common/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
   -- the colorscheme should be available when starting Neovim
   {
-    'rebelot/kanagawa.nvim',
+    "neanias/everforest-nvim",
+    version = false,
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme kanagawa]])
+      vim.cmd([[colorscheme everforest]])
     end,
   },
+  {
 }
