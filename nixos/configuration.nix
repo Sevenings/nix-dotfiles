ac4c5788ecdd274f140efb61c6204dbc8116a542
Sevenings <lvlassis.2@gmail.com>
2025-02-28 07:57:18 -0300

    fix: Remove Droid-sans-mono

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -64,9 +64,8 @@
   # fonts.packages = [] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
   # Before 25.05 (24.05 or earlier)
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
     fira-code
-    droid-sans-mono
   ];
 
 
