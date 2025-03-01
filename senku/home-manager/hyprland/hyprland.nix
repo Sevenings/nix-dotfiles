1bd6207a18464daf6b088b4c00101ed22151a4f8
Sevenings <lvlassis.2@gmail.com>
2025-03-01 18:41:21 -0300

    fix:

--- senku/home-manager/hyprland/hyprland.nix
+++ senku/home-manager/hyprland/hyprland.nix
@@ -23,7 +23,7 @@
     # Monitor configuration
     monitor = [
       # Monitor principal
-      "DP-1, highres, 0x0@180, 1"
+      "DP-1, highres, 0x0, 1"
 
       # Meu segundo monitor
       "HDMI-A-1, highres, 1920x0, 1"	
