359f1dfabd7ab6516e70fc409c045c812d9f49bf
Sevenings <lvlassis.2@gmail.com>
2025-03-02 10:45:24 -0300

    refactor: waybar migrado para nix

--- okabe/home-manager/waybar.nix
+++ okabe/home-manager/waybar.nix
@@ -3,11 +3,17 @@
 {
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  # home.file = {
     # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # symlink to the Nix store copy.
-    ".config/waybar".source = ./waybar;
+    # ".config/waybar".source = ./waybar;
+  # };
+
+  programs.waybar = {
+    enable = true;
+    settings = builtins.fromJSON ( builtins.readFile ./waybar/gruvbox/config.json );
+    style = ./waybar/gruvbox/style.css;
   };
 
 }
