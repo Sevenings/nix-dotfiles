fbdffdea1d6c86940260e5629bbd9bec09039806
Sevenings <lvlassis.2@gmail.com>
2025-03-04 12:41:16 -0300

    install inkscape

--- senku/nixos/packages.nix
+++ senku/nixos/packages.nix
@@ -4,6 +4,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
     environment.systemPackages = with pkgs; [
+      inkscape
       cargo
       nodePackages.live-server
       postman
