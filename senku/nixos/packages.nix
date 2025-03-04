7de4ce0c06fd63a567f43a95d15852d504db0b45
Sevenings <lvlassis.2@gmail.com>
2025-03-04 14:07:24 -0300

    feat: installed ferium

--- senku/nixos/packages.nix
+++ senku/nixos/packages.nix
@@ -4,6 +4,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
     environment.systemPackages = with pkgs; [
+      ferium
       inkscape
       cargo
       nodePackages.live-server
