91c5adbb69173b7307eaa4a6ecf3c9fe09b5f513
Sevenings <lvlassis.2@gmail.com>
2025-03-10 10:15:24 -0300

    commit

--- senku/home-manager/hyprland/appearance.nix
+++ senku/home-manager/hyprland/appearance.nix
@@ -49,7 +49,7 @@
                 "border, 1, 10, default"
                 "borderangle, 1, 8, default"
                 "fade, 1, 3, default"
-                "workspaces, 1, 4, default, slidevert"
+                "workspaces, 1, 4, Abezier, slidevert"
                 "specialWorkspace, 1, 3, easeOutBack, slidevert"
             ];
         };
