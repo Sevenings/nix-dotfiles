f90eaf63d72ca436ab59b722d95b74ab842b4b76
Sevenings <lvlassis.2@gmail.com>
2025-03-10 11:09:46 -0300

    commit

--- senku/home-manager/kitty.nix
+++ senku/home-manager/kitty.nix
@@ -10,7 +10,7 @@
     };
     settings = {
       background_opacity = 0.75;
-      confirm_os_window_close = false;
+      confirm_os_window_close = 0;
       enable_audio_bell = false;
     };
   };
