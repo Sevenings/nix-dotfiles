28c8997e4c6c8c0483fb8b42aa503e13c5c1b3bd
Sevenings <lvlassis.2@gmail.com>
2025-03-10 10:25:25 -0300

    quick commit

--- senku/home-manager/hyprland/plugins.nix
+++ senku/home-manager/hyprland/plugins.nix
@@ -4,7 +4,7 @@
 
   # Installed Plugins
   wayland.windowManager.hyprland.plugins = [
-	# pkgs.hyprlandPlugins.hyprbars
+	  pkgs.hyprlandPlugins.hyprbars
 	  # pkgs.hyprlandPlugins.hyprgrass
   ];
 
