661fe13a69d4e8733e1ea873beb7b1c292f3fb54
Sevenings <lvlassis.2@gmail.com>
2025-03-10 11:08:43 -0300

    commit

--- senku/home-manager/kitty.nix
+++ senku/home-manager/kitty.nix
@@ -8,5 +8,10 @@
       name = "Source Code Pro";
       size = 13.0;
     };
+    settings = {
+      background_opacity = 0.75;
+      confirm_os_window_close = false;
+      enable_audio_bell = false;
+    };
   };
 }
