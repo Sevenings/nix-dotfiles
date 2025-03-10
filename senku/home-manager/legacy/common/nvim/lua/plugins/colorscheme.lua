fdc4b76a43a549dcaa4233c85144ddc81f25dae8
Sevenings <lvlassis.2@gmail.com>
2025-03-10 11:23:34 -0300

    fix

--- senku/home-manager/legacy/common/nvim/lua/plugins/colorscheme.lua
+++ senku/home-manager/legacy/common/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 return {
   -- the colorscheme should be available when starting Neovim
   {
-    "neanias/everforest-nvim",
+    "sainnhe/sonokai",
     version = false,
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require('everforest').setup({
-        background = "hard"
-      })
+      -- require('everforest').setup({
+      --   background = "hard"
+      -- })
       -- load the colorscheme here
-      vim.cmd([[colorscheme everforest]])
+      vim.cmd([[colorscheme sonokai]])
     end,
   }
 }
