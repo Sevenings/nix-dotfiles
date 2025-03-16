80b651cee1ca88bfcd93bb09d904589c94cea340
Sevenings <lvlassis.2@gmail.com>
2025-03-16 17:02:52 -0300

    fix waybar no startup

--- common/home-manager/hyprland/auto-start.nix
+++ common/home-manager/hyprland/auto-start.nix
@@ -5,11 +5,12 @@
 
         # Execute your favorite apps at launch
         "exec-once" = [
-            "swaync"
-            "/usr/lib/polkit-gnome-authentication-agent-1"
-            # "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-            "hyprpaper"
-            "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+          "waybar"
+          "swaync"
+          "/usr/lib/polkit-gnome-authentication-agent-1"
+          # "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+          "hyprpaper"
+          "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         ];
 
     };
