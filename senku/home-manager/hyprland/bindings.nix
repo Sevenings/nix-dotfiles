4ebd368292bfb26eea951530bf42580f86d18434
Sevenings <lvlassis.2@gmail.com>
2025-03-17 09:39:30 -0300

    fix: inverte as bindings de ctrl J e K

--- senku/home-manager/hyprland/bindings.nix
+++ senku/home-manager/hyprland/bindings.nix
@@ -4,8 +4,8 @@
   wayland.windowManager.hyprland.settings = {
 
     bind = [
-      "$mainMod CTRL, J, workspace, -1"
-      "$mainMod CTRL, K, workspace, +1"
+      "$mainMod CTRL, J, workspace, +1"
+      "$mainMod CTRL, K, workspace, -1"
     ];
   
   };
