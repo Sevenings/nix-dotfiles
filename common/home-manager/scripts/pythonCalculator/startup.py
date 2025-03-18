19c139dcee0a9d86050886dd31c0b8da3ca45223
Sevenings <lvlassis.2@gmail.com>
2025-03-18 10:21:10 -0300

    python calculator edits

--- common/home-manager/scripts/pythonCalculator/startup.py
+++ common/home-manager/scripts/pythonCalculator/startup.py
@@ -3,13 +3,21 @@
 "------------------"
 
 import os
-from math import *
+from math import sqrt
+
+
+"-------------------"
+"Circuitos Eletricos"
+"-------------------"
 
 def baskara(a, b, c):
     delta = b**2 - 4*a*c
     x1 = (-b - sqrt(delta)) / 2*a
     x2 = (-b + sqrt(delta)) / 2*a
     return x1, x2
+
+def hipotenusa(b, c):
+    return sqrt(b**2 + c**2)
 
 
 "-------------------"
@@ -20,10 +28,10 @@ def paralelo(a, b):
     return a*b/(a+b)
 
 
-
 "-------------"
 "    Title    "
 "-------------"
 
 os.system('clear')
 print("Zenitsu's Python Calculator")
+
