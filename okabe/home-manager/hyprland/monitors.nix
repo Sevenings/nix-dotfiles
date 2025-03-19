cec326cc5f0da1655e6a3558d31a244fbd197b16
Sevenings <lvlassis.2@gmail.com>
2025-03-19 14:00:38 -0300

    change: coloca o monitor do estágio na esquerda

---
+++ okabe/home-manager/hyprland/monitors.nix
@@ -0,0 +1,19 @@
+{ ... }:
+
+{
+    wayland.windowManager.hyprland.settings = {
+
+      # Monitor configuration
+      monitor = [
+        # Monitor principal
+        "eDP-1, highres, 0x0, 1"	
+
+        # Monitor do estágio
+        "desc:Dell Inc. DELL D2216H 70GJH69U1SII, highres, -1920x0, 1"	
+
+        # Outros monitores: espelhados por padrão
+        ", highres, 0x1080, 1, mirror, eDP-1"  
+      ];
+
+    };
+}
