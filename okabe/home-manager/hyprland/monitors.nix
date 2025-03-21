d280fca8a70c6639e39dbb0d4a29cb79b4e86e8f
Sevenings <lvlassis.2@gmail.com>
2025-03-21 13:17:47 -0300

    fix(okabe): Volta o monitor do estágio para cima

--- okabe/home-manager/hyprland/monitors.nix
+++ okabe/home-manager/hyprland/monitors.nix
@@ -9,7 +9,7 @@
         "eDP-1, highres, 0x0, 1"	
 
         # Monitor do estágio
-        "desc:Dell Inc. DELL D2216H 70GJH69U1SII, highres, -1920x0, 1"	
+        "desc:Dell Inc. DELL D2216H 70GJH69U1SII, highres, 0x-1080, 1"	
 
         # Outros monitores: espelhados por padrão
         ", highres, 0x1080, 1, mirror, eDP-1"  
