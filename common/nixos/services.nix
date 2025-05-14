28c6a9b2db4fd8badb5568d0ce94c7a679fc8248
Sevenings <lvlassis.2@gmail.com>
2025-05-14 16:32:40 -0300

    feat: Instala o flatpack

--- common/nixos/services.nix
+++ common/nixos/services.nix
@@ -2,10 +2,14 @@
 
 {
   services = {
+
     xserver = {
       enable = true;
       xwayland.enable = true;
       displayManager.session = "wayland";
     };
+
+    flatpak.enable = true;
+
   };
 }
