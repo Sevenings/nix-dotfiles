8a8a02e24610de8d432ca9f35659fd320b5b6eea
Sevenings <lvlassis.2@gmail.com>
2025-05-27 19:04:59 -0300

    fix: focusmode rounding

--- common/home-manager/scripts/focusmode.sh
+++ common/home-manager/scripts/focusmode.sh
@@ -6,5 +6,5 @@ killall -SIGUSR1 waybar
 hyprctl keyword general:gaps_in 0
 hyprctl keyword general:gaps_out 0
 hyprctl keyword general:border_size 1
-hyprctl keyword decoration:rouding 0
+hyprctl keyword decoration:rounding 0
 
