42385095509e782af00f35a43dce583053b18473
Sevenings <lvlassis.2@gmail.com>
2025-07-22 11:51:08 -0300

    tentativas configurar mpd

--- common/nixos/services.nix
+++ common/nixos/services.nix
@@ -7,8 +7,26 @@
 
     pipewire = {
       enable = true;
+      alsa.enable = true;
+      jack.enable = true;
       pulse.enable = true;
+      wireplumber.enable = true;
     };
 
+    mpd = {
+      enable = true;
+      user = "okabe";
+      musicDirectory = "/home/okabe/Musicas";
+      network.listenAddress = "any";
+      startWhenNeeded = false;
+
+      extraConfig = ''
+        audio_output {
+          type        "pulse"
+          name        "PipeWire Pulse"
+          server      "127.0.0.1"
+        }
+      '';
+    };
   };
 }
