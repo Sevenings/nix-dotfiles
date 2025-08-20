750df9826aa30781ec745273dbe3dad0027e3c2e
Sevenings <lvlassis.2@gmail.com>
2025-08-20 08:08:29 -0300

    install: pacotes de scripts

--- common/home-manager/packages.nix
+++ common/home-manager/packages.nix
@@ -2,7 +2,13 @@
 
 {
   home.packages = with pkgs; [
+    babel
+    colorpicker
     musicnow
+    python_calculator
+    switchlayout
+    togglecaps
+    workspace
     freecad-wayland
     rustc
     cargo
