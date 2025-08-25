4188901944b366695246fc172682c77970d99cea
Sevenings <lvlassis.2@gmail.com>
2025-08-25 20:44:18 -0300

    fix: arruma o path do wallpaper do python_calculator
    fix: atualiza o path do wallpaper do python_calculator

--- misc/pkgs/python_calculator/python_calculator.sh
+++ misc/pkgs/python_calculator/python_calculator.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 terminal="kitty"
-calculatorKittyConfig="$HOME/.dotfiles/misc/pkgs/pythonCalculator/calculatorConfig/kitty.conf"
+calculatorKittyConfig="$HOME/.dotfiles/misc/pkgs/python_calculator/calculatorConfig/kitty.conf"
 calculator="$terminal \
     --class=floatingTerminal \
     --config=$calculatorKittyConfig \
     -e python3"
 
-export PYTHONSTARTUP=$HOME/.dotfiles/misc/pkgs/pythonCalculator/startup.py
+export PYTHONSTARTUP=$HOME/.dotfiles/misc/pkgs/python_calculator/startup.py
 $calculator
 
