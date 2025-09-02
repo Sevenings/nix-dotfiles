b5ae77aec6ea0378b46bbd590ce3e1a3dbca647b
Sevenings <lvlassis.2@gmail.com>
2025-09-01 22:27:34 -0300

    feat: implementa se_expediente corretamente

--- misc/pkgs/se_expediente/se_expediente.sh
+++ misc/pkgs/se_expediente/se_expediente.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-echo "Hello World se_expediente.sh"
+# Obtém o SSID atual
+ssid=$(nmcli -t -f active,ssid dev wifi | grep "^sim" | cut -d: -f2)
+
+if [[ "$ssid" == "TCEGO-PUBLIC" ]]; then
+    echo "Está no estágio"
+    exit 0
+fi
+echo "Não está no estágio"
+exit 1
