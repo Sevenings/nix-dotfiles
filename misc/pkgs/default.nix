98e1e75913ad3cc0596a281d0b199667d78ff3fb
Sevenings <lvlassis.2@gmail.com>
2025-10-06 11:27:51 -0300

    refact: misc/pkgs fica automatico

--- misc/pkgs/default.nix
+++ misc/pkgs/default.nix
@@ -1,18 +1,16 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: {
-  babel = pkgs.callPackage ./babel {};
-  colorpicker = pkgs.callPackage ./colorpicker {};
-  focusmode = pkgs.callPackage ./focusmode {};
-  horario_ponto = pkgs.callPackage ./horario_ponto {};
-  musicnow = pkgs.callPackage ./musicnow {};
-  openCalendar = pkgs.callPackage ./openCalendar {};
-  python_calculator = pkgs.callPackage ./python_calculator {};
-  reload_wallpaper = pkgs.callPackage ./reload_wallpaper {};
-  reload_waybar = pkgs.callPackage ./reload_waybar {};
-  se_expediente = pkgs.callPackage ./se_expediente {};
-  switchlayout = pkgs.callPackage ./switchlayout {};
-  togglecapslock = pkgs.callPackage ./togglecapslock {};
-  workspace = pkgs.callPackage ./workspace {};
-  zapdos = pkgs.callPackage ./zapdos {};
-}
+pkgs:
+let
+  # Get all directories in current directory that contain a default.nix file
+  packageDirs = builtins.filter (dir: 
+    builtins.pathExists (./. + "/${dir}/default.nix")
+  ) (builtins.attrNames (builtins.readDir ./.));
+  
+  # Create an attribute set from the package directories
+  packages = builtins.listToAttrs (map (dir: {
+    name = dir;
+    value = pkgs.callPackage (./. + "/${dir}") {};
+  }) packageDirs);
+in
+packages
