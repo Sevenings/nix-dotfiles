189507e77695b91d9a9c8a6012e71938d45ea06f
Sevenings <lvlassis.2@gmail.com>
2025-12-01 18:50:08 -0300

    termfilechooser

--- common/home-manager/xdg.nix
+++ common/home-manager/xdg.nix
@@ -5,13 +5,12 @@
     portal = {
       enable = true;
       extraPortals = with pkgs; [
-        xdg-desktop-portal-hyprland
         xdg-desktop-portal-gtk
         xdg-desktop-portal-termfilechooser
       ];
-    };
-    config.common = {
-      "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+      config.common = {
+        "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+      };
     };
   };
 }
