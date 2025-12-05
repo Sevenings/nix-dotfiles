ba47d2414b91a4489ba9f788c4a539800449a53d
Sevenings <lvlassis.2@gmail.com>
2025-12-05 14:57:31 -0300

    fonts: instala corefonts

--- common/nixos/fonts.nix
+++ common/nixos/fonts.nix
@@ -6,6 +6,7 @@
   fonts.packages = with pkgs; [
     anonymousPro
     victor-mono
+    corefonts # Inclui Times New Roman
   ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
 }
 
