33753941f955222333230a1ffac12cc8d10fb263
Lucas Vinícius de L. Assis <lvlassis.2@gmail.com>
2026-07-04 16:10:23 -0300

    install: yazi tinty theme

--- common/home-manager/tinty/default.nix
+++ common/home-manager/tinty/default.nix
@@ -4,4 +4,15 @@
   home.packages = with pkgs; [
     tinty
   ];
+
+  home.file.".config/tinted-theming/tinty/config.toml".text = ''
+    [[items]]
+    path = "https://github.com/tinted-theming/tinted-yazi"
+    name = "tinted-yazi"
+    themes-dir = "flavors"
+    hook = ''''
+    mkdir -p "$HOME/.config/yazi/flavors/tinted-scheme.yazi"
+    cp -f "$TINTY_THEME_FILE_PATH" "$HOME/.config/yazi/flavors/tinted-scheme.yazi/flavor.toml"
+    ''''
+  '';
 }
