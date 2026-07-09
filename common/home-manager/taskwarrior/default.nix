b24c2ed06bdce244f028dc4cca94a975b65b6ae1
Sevenings <lvlassis.2@gmail.com>
2026-07-09 13:55:21 -0300

    refactor(task-sync): utiliza servidor público tasks.sevening.com.br

--- common/home-manager/taskwarrior/default.nix
+++ common/home-manager/taskwarrior/default.nix
@@ -4,7 +4,7 @@
     enable = true;
     package = pkgs.taskwarrior3;
     config = {
-      sync.server.url="http://tasks.bebop.lan";
+      sync.server.url="https://tasks.sevening.com.br";
       sync.server.client_id="852c396d-987f-43fa-bd72-f2d85a3572ed";
       sync.encryption_secret="lucassupersecret";
     };
