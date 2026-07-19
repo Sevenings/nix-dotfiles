31f9cb75b982f3fc563a9b9d24564a0e8a03d319
Sevenings <lvlassis.2@gmail.com>
2026-07-19 12:02:58 -0300

    fix(taskwarrior): adiciona contextos

--- common/home-manager/taskwarrior/default.nix
+++ common/home-manager/taskwarrior/default.nix
@@ -7,6 +7,12 @@
       sync.server.url="https://tasks.sevening.com.br";
       sync.server.client_id="852c396d-987f-43fa-bd72-f2d85a3572ed";
       sync.encryption_secret="lucassupersecret";
+      context.servico_ti.read="+servico_ti";
+      context.servico_ti.write="+servico_ti";
+      context.pessoal.read="+pessoal";
+      context.pessoal.write="+pessoal";
+      context.faculdade.read="+faculdade";
+      context.faculdade.write="+faculdade";
     };
   };
 }
